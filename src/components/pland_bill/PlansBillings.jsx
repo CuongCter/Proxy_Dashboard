42f8e9db529f51d376d2a5350be67fbc0633a228
CuongCter <cuongcter442002@gmail.com>
2023-06-09 11:01:28 +0700

    coding header plan_billing

--- src/components/pland_bill/PlansBillings.jsx
+++ src/components/pland_bill/PlansBillings.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../assets/scss/components/_plansBillings.scss'
+import IconSelect from '../../assets/images/Select.png'
+import IconSearch from '../../assets/images/Search.png'
+import Plans from '../plans/Plans'
+import Billings from '../billings/Billings'
 const PlansBillings = () => {
+  const [activeChoose, setActiveChoose] = useState("plans")
+
   return (
     <div className='planBilling'>
         <div className="planBilling__header">
             <div className='planBilling__header-choose'>
-                <button>Plans</button>
-                <button>Billings</button>
+                <div className={`planBilling__header-choose-btn ${activeChoose === "plans" ? "activeChoose" : ""}`}
+                  onClick={()=>setActiveChoose("plans")}
+                >Plans</div>
+                <div className={`planBilling__header-choose-btn ${activeChoose === "billings" ? "activeChoose" : ""}`}
+                  onClick={()=>setActiveChoose("billings")}
+                >Billings</div>
             </div>
             <div className="planBilling__header-filter">
-                <h3>Filter</h3>
-                <div>None</div>
-                <div>Asc</div>
-                <input type="text" placeholder='Search' />
+                <h3 className='planBilling__header-filter-title'>Filter:</h3>
+                <div className='planBilling__header-filter-none'>
+                  <div>None</div>
+                  <img src={IconSelect} alt="" />
+                </div>
+                <div className='planBilling__header-filter-asc'>
+                  <div>Asc</div>
+                  <img src={IconSelect} alt="" />
+                </div>
+                <div className='planBilling__header-filter-input'>
+                  <img src={IconSearch} className='planBilling__header-filter-iconsearch' alt="" />
+                  <input type="text" placeholder='Search' className='planBilling__header-filter-search' />
+                </div>         
             </div>
+        </div>
+        <div className='planBilling__table'>
+          
+          {
+            activeChoose === "plans" ? <Plans></Plans> : <Billings></Billings>
+          }
         </div>
     </div>
   )
