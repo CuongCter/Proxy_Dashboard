ab8affb371682d614784ee74907f256fc1d7feb0
CuongCter <cuongcter442002@gmail.com>
2023-06-10 00:47:03 +0700

    table plan billing

--- src/components/billings/Billings.jsx
+++ src/components/billings/Billings.jsx
@@ -2,7 +2,37 @@
     
     const Billings = () => {
       return (
-        <div>Billings</div>
+        <div className='plan'>
+        <table class="plan__table">
+            <thead className='plan__table-header'>
+                <tr >
+                    <th>ID</th>
+                    <th>Amount</th>
+                    <th>Bandwitdh</th>
+                    <th>Buy Time</th>
+                    <th>Type</th>
+
+                </tr>
+            </thead>
+            <tbody className='plan__table-body'>      
+                <tr>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                    <td>Data 3</td>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+
+                </tr>
+                <tr>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                    <td>Data 3</td>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
       )
     }
     
--- src/components/plans/Plans.jsx
+++ src/components/plans/Plans.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
-
+import '../../assets/scss/components/_plans.scss'
 const Plans = () => {
-  return (
-    <div>Plans</div>
-  )
+    return (
+        <div className='plan'>
+            <table class="plan__table">
+                <thead className='plan__table-header'>
+                    <tr >
+                        <th>Order ID</th>
+                        <th>Total Bandwidth</th>
+                        <th>Used Bandwitdh</th>
+                        <th>Total Request</th>
+                        <th>Buy Time</th>
+                        <th>Expire Time</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody className='plan__table-body'>      
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 3</td>
+                    </tr>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Plans
